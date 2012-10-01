b1608b16183cb6500b9b07d1adc50bdd8a679b27
Nicky <nicky_qianlei@163.com>
2012-10-01 21:57:39 +0800

    add Struts2 framework

---
+++ src/main/java/com/ebay/nearby/action/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.ebay.nearby.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorld extends  ActionSupport  {	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public String execute() throws Exception {
+
+		return SUCCESS;
+
+		}
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,11 @@
 <html>
+<head>
+<title>hello</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+	<h2>Hello World!</h2>
+	<form action="test" method="post">
+	<input type="submit" />
+	</form>
 </body>
 </html>
